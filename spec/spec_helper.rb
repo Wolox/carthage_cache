a046e885096284d6964bc0b6e3d1a1efd124782f
Guido Marucci Blas <guidomb@gmail.com>
2016-03-14 00:54:19 -0300

    Moves CodeClimate runner at beginning of file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'carthage_cache'
 require_relative './mocks/mock_terminal'
 require_relative './mocks/mock_command_executor'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 FIXTURE_PATH = File.expand_path('../fixtures/project', __FILE__)
 TMP_PATH = File.expand_path('../fixtures/tmp', __FILE__)
