1934b84a94225e3c51fab9f50cde75ada66a239c
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-05-04 23:51:47 -0300

    Load AWS_PROFILE env var

--- lib/carthage_cache/configuration.rb
+++ lib/carthage_cache/configuration.rb
@@ -25,7 +25,8 @@ module CarthageCache
         aws_s3_client_options: {
           region: ENV['AWS_REGION'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+          profile: ENV['AWS_PROFILE']
         },
         tmpdir: Dir.tmpdir
       })
