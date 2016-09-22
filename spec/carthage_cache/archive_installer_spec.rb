9b646e11e461e7854e7f70e5c1469d84224d2671
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-22 11:59:51 +1000

    Add test ensuring installer actually unarchives zip
    This will be meaningful once we'll implement the check for the zip in
    the local cache, as we'll ensure that the archive won't be downloaded
    again

--- spec/carthage_cache/archive_installer_spec.rb
+++ spec/carthage_cache/archive_installer_spec.rb
@@ -14,7 +14,11 @@ describe CarthageCache::ArchiveInstaller do
     let(:archive_path) { File.join(project.tmpdir, project.archive_filename) }
 
     it "downloads and installs the archive" do
+      # download expectation
       expect(repository).to receive(:download).with(project.archive_filename, archive_path)
+      # install expectation
+      expect(archiver).to receive(:unarchive).with(archive_path, project.carthage_build_directory)
+
       archive_installer.install
     end
 
