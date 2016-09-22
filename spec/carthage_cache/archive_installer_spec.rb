e79edbfaef65d256f73b4004eb1b29a502fde5d1
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-22 15:18:16 +1000

    Only download archive if not present in local cache

--- spec/carthage_cache/archive_installer_spec.rb
+++ spec/carthage_cache/archive_installer_spec.rb
@@ -11,15 +11,43 @@ describe CarthageCache::ArchiveInstaller do
 
   describe "#install" do
 
-    let(:archive_path) { File.join(project.tmpdir, project.archive_filename) }
+    context "when there is no copy of the archive in the local cache" do
 
-    it "downloads and installs the archive" do
-      # download expectation
-      expect(repository).to receive(:download).with(project.archive_filename, archive_path)
-      # install expectation
-      expect(archiver).to receive(:unarchive).with(archive_path, project.carthage_build_directory)
+      let(:project) { CarthageCache::Project.new(FIXTURE_PATH, cache_dir_name, terminal, TMP_PATH) }
+      let(:archive_path) { File.join(project.tmpdir, project.archive_filename) }
 
-      archive_installer.install
+      it "downloads and installs the archive" do
+        # download expectation
+        expect(repository).to receive(:download).with(project.archive_filename, archive_path)
+        # install expectation
+        expect(archiver).to receive(:unarchive).with(archive_path, project.carthage_build_directory)
+
+        archive_installer.install
+      end
+
+    end
+
+    context "when there is a copy of the archive in the local cache" do
+
+      let(:project) { CarthageCache::Project.new(FIXTURE_WITH_CACHE_PATH, cache_dir_name, terminal, TMP_PATH) }
+      let(:archive_path) { File.join(project.tmpdir, project.archive_filename) }
+
+      before do
+        FileUtils.cp(File.join(FIXTURE_ARCHIVE_PATH, "archive.zip"), archive_path)
+      end
+
+      after do
+        FileUtils.rm(archive_path)
+      end
+
+      it "installs the archive without downloading it" do
+        # without downloading expectation
+        expect(repository).to_not receive(:download)
+        # install expectation
+        expect(archiver).to receive(:unarchive).with(archive_path, project.carthage_build_directory)
+
+        archive_installer.install
+      end
     end
 
   end
