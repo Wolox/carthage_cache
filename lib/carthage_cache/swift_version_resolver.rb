1b0a7364e2d8ec07ec480da2778eccf80d980b5a
Luke Wakeford <luke.wakeford@passengerteam.com>
2021-09-17 14:56:41 +0100

    updates version resolver as broken in Xcode 13

--- lib/carthage_cache/swift_version_resolver.rb
+++ lib/carthage_cache/swift_version_resolver.rb
@@ -8,7 +8,7 @@ module CarthageCache
 
     def swift_version
       output = @executor.execute('xcrun swift -version').chomp
-      version_string = /(\d+\.)?(\d+\.)?(\d+)/.match(output).to_s
+      version_string = /(([0-9])+(\.){0,1})+/.match(output).to_s
       Gem::Version.new(version_string)
     end
 
